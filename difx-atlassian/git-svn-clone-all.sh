4fb2eb42adb38c15ff1acdceae61b6cda96e36f0
Greg Lindahl <lindahl@pbm.com>
2020-09-23 08:27:15 -0700

    initial attempts

---
+++ difx-atlassian/git-svn-clone-all.sh
@@ -0,0 +1,13 @@
+git svn clone --stdlayout --authors-file=authors.txt https://svn.atnf.csiro.au/difx/applications/DiFXGUI difx/applications/DiFXGUI
+git svn clone --stdlayout --authors-file=authors.txt https://svn.atnf.csiro.au/difx/applications/calc difx/applications/calc
+git svn clone --stdlayout --authors-file=authors.txt https://svn.atnf.csiro.au/difx/applications/calcserver difx/applications/calcserver
+git svn clone --stdlayout --authors-file=authors.txt https://svn.atnf.csiro.au/difx/applications/difx2fits difx/applications/difx2fits
+git svn clone --stdlayout --authors-file=authors.txt https://svn.atnf.csiro.au/difx/applications/difx2ms difx/applications/difx2ms
+git svn clone --stdlayout --authors-file=authors.txt https://svn.atnf.csiro.au/difx/applications/difx_monitor difx/applications/difx_monitor
+git svn clone --stdlayout --authors-file=authors.txt https://svn.atnf.csiro.au/difx/applications/mk5daemon difx/applications/mk5daemon
+
+git svn clone --stdlayout --authors-file=authors.txt https://svn.atnf.csiro.au/difx/deprecated/fringetool difx/deprecated/fringetool
+git svn clone --stdlayout --authors-file=authors.txt https://svn.atnf.csiro.au/difx/deprecated/vlba_utils difx/deprecated/vlba_utils
+
+git svn clone --stdlayout --authors-file=authors.txt https://svn.atnf.csiro.au/difx/mpifxcorr difx/mpifxcorr
+git svn clone --stdlayout --authors-file=authors.txt https://svn.atnf.csiro.au/difx/utilities difx/utilities
