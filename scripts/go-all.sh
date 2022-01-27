cd0ceea17b25d053fa42d3ed7279564b14f6735f
Shaoguang Guo(Sky & Star) <sgguo@shao.ac.cn>
2022-01-27 15:55:35 +0800

    fixed un-cloned repo and non-stdlayout repos

--- scripts/go-all.sh
+++ scripts/go-all.sh
@@ -2,7 +2,45 @@
 
 #rm -rvf $1
 
-for repo in difx/applications/calcif2 difx/applications/datasim difx/applications/difx2mark4 difx/applications/difxcalc11 difx/applications/espresso difx/applications/hops difx/applications/mk5daemon difx/applications/vex2difx h difx/applications/calcserver difx/applications/difx2fits difx/applications/difx_monitor difx/applications/difxfilterbank difx/applications/guiServer difx/applications/m6support difx/applications/polconvert difx/libraries/codifio difx/libraries/difxio difx/libraries/difxmessage difx/libraries/dirlist difx/libraries/mark5access difx/libraries/mark6meta difx/libraries/mark6sg difx/libraries/perl difx/libraries/python difx/libraries/vdifio difx/libraries/vex difx/mpifxcorr difx/sites/MPIfR difx/sites/NRAO difx/tests/ATNF difx/tests/Synthetic difx/utilities; do
+for repo in difx/applications/datasim\
+ difx/applications/difx2mark4 \
+ difx/applications/difxcalc11 \
+ difx/applications/espresso \
+ difx/applications/hops \
+ difx/applications/mk5daemon \
+ difx/applications/vex2difx\
+ difx/applications/calcserver \ 
+ difx/applications/difx2fits \
+ difx/applications/difx_monitor \ 
+ difx/applications/difxfilterbank \ 
+ difx/applications/guiServer \
+ difx/applications/m6support \
+ difx/applications/polconvert \
+ difx/libraries/codifio \
+ difx/libraries/difxio \
+ difx/libraries/difxmessage \ 
+ difx/libraries/dirlist \
+ difx/libraries/mark5access\ 
+ difx/libraries/mark6meta \
+ difx/libraries/mark6sg \
+ difx/libraries/perl \
+ difx/libraries/python \ 
+ difx/libraries/vdifio \
+ difx/libraries/vex \
+ difx/mpifxcorr \
+ difx/sites/NRAO \
+ difx/utilities \
+ difx/applications/DiFXGUI \
+ difx/applications/calc \
+ difx/applications/difx2ms\ 
+ difx/applications/doi \
+ difx/applications/gui \
+ difx/applications/pydifx \ 
+ difx/applications/vdif_server \
+ difx/libraries/beamformer \
+ difx/libraries/difxbase \
+ difx/libraries/rpfits 
+ do
 	if [ ! -d $repo ]; then
 		bash go-svn-repo.sh $repo
 	else
@@ -10,3 +48,16 @@ for repo in difx/applications/calcif2 difx/applications/datasim difx/application
 		echo 'repo exists, delete to re-run or just skip'
 	fi
 done
+
+# calcif2/MPIfR/ATNF/Synthetis is not stdlayout
+for repo in difx/applications/calcif2 difx/tests/ATNF; do
+	if [ ! -d $repo ]; then
+		bash go-svn-repo.sh $repo
+	else
+		echo $repo
+		echo 'repo exists, delete to re-run or just skip'
+	fi
+done
+
+### FAILED
+###difx/applications/calcif2 difx/tests/ATNF
