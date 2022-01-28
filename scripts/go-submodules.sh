2e19f482085e8d1dc989e85b1927f66fa6e4dab7
Shaoguang Guo(Sky & Star) <sgguo@shao.ac.cn>
2022-01-28 15:19:22 +0800

    Trying to map external in svn to submodule in git

---
+++ scripts/go-submodules.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/bash
+git submodule add http://192.168.254.229/difx/applications/calcif2         applications/calcif2  
+git submodule add http://192.168.254.229/difx/applications/datasim         applications/datasim 
+git submodule add http://192.168.254.229/difx/applications/difx2mark4      applications/difx2mark4  
+git submodule add http://192.168.254.229/difx/applications/difxcalc11      applications/difxcalc11  
+git submodule add http://192.168.254.229/difx/applications/espresso        applications/espresso  
+git submodule add http://192.168.254.229/difx/applications/hops            applications/hops  
+git submodule add http://192.168.254.229/difx/applications/mk5daemon       applications/mk5daemon  
+git submodule add http://192.168.254.229/difx/applications/vex2difx        applications/vex2difx 
+git submodule add http://192.168.254.229/difx/applications/calcserver      applications/calcserver   
+git submodule add http://192.168.254.229/difx/applications/difx2fits       applications/difx2fits  
+git submodule add http://192.168.254.229/difx/applications/difx_monitor    applications/difx_monitor  
+git submodule add http://192.168.254.229/difx/applications/difxfilterbank  applications/difxfilterbank 
+git submodule add http://192.168.254.229/difx/applications/guiServer       applications/guiServer  
+git submodule add http://192.168.254.229/difx/applications/m6support       applications/m6support  
+git submodule add http://192.168.254.229/difx/applications/polconvert      applications/polconvert  
+git submodule add http://192.168.254.229/difx/applications/DiFXGUI         applications/DiFXGUI  
+git submodule add http://192.168.254.229/difx/applications/calc            applications/calc  
+git submodule add http://192.168.254.229/difx/applications/difx2ms         applications/difx2ms  
+git submodule add http://192.168.254.229/difx/applications/doi             applications/doi  
+git submodule add http://192.168.254.229/difx/applications/gui             applications/gui  
+git submodule add http://192.168.254.229/difx/applications/pydifx          applications/pydifx   
+git submodule add http://192.168.254.229/difx/applications/vdif_server     applications/vdif_server  
+
+git submodule add http://192.168.254.229/difx/libraries/codifio            libraries/codifio  
+git submodule add http://192.168.254.229/difx/libraries/difxio             libraries/difxio  
+git submodule add http://192.168.254.229/difx/libraries/difxmessage        libraries/difxmessage 
+git submodule add http://192.168.254.229/difx/libraries/dirlist            libraries/dirlist  
+git submodule add http://192.168.254.229/difx/libraries/mark5access        libraries/mark5access 
+git submodule add http://192.168.254.229/difx/libraries/mark6meta          libraries/mark6meta  
+git submodule add http://192.168.254.229/difx/libraries/mark6sg            libraries/mark6sg  
+git submodule add http://192.168.254.229/difx/libraries/perl               libraries/perl  
+git submodule add http://192.168.254.229/difx/libraries/python             libraries/python   
+git submodule add http://192.168.254.229/difx/libraries/vdifio             libraries/vdifio  
+git submodule add http://192.168.254.229/difx/libraries/vex                libraries/vex  
+git submodule add http://192.168.254.229/difx/libraries/beamformer         libraries/beamformer  
+git submodule add http://192.168.254.229/difx/libraries/difxbase           libraries/difxbase  
+git submodule add http://192.168.254.229/difx/libraries/rpfits             libraries/rpfits      
+
+git submodule add http://192.168.254.229/difx/mpifxcorr 		mpifxcorr 
+git submodule add http://192.168.254.229/difx/sites/NRAO  		sites/NRAO
+git submodule add http://192.168.254.229/difx/utilities 		utilities 
+git submodule add http://192.168.254.229/difx/tests/ATNF		tests/ATNF
+### FAILED
+###difx/applications/calcif2 difx/tests/ATNF
